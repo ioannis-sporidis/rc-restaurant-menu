2a2bd2709604a394e838398067e4e4aa48c67d08
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-07-24 08:04:05 -0400

    Initial static elements setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import { useState } from 'react';
+import items from './data';
+import Menu from './components/Menu';
+import Categories from './components/Categories';
 
 function App() {
-  return <></>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        <Menu />
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Categories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Categories = () => {
+  return <div>Categories Component</div>;
+};
+
+export default Categories;
