21fa077ff44480fde3c62eebed65e292b5736909
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-07-26 20:46:40 -0400

    Added category buttons - hard coded

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const Categories = () => {
-  return <div>Categories Component</div>;
+const Categories = ({ filterItems }) => {
+  return (
+    <div className='btn-container'>
+      <button
+        type='button'
+        className='filter-btn'
+        onClick={() => {
+          filterItems('all');
+        }}>
+        All
+      </button>
+      <button
+        type='button'
+        className='filter-btn'
+        onClick={() => {
+          filterItems('breakfast');
+        }}>
+        Breakfast
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
